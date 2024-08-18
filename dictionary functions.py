b806c0f1d025925de85c2a2e03df983be49c7fb6
Hardik Prajapat <135196687+Hardikprajapat14@users.noreply.github.com>
2024-08-18 20:29:33 +0530

    Add files via upload

---
+++ dictionary functions.py
@@ -0,0 +1,17 @@
+ep1={1: 45, 2: 50, 3: 67, 4: 70}
+ep2={7: 90, 8: 91, 9: 45}
+
+#update_command 
+#ep1.update(ep2)
+
+#clear_command
+#ep1.clear()
+
+#pop_cmnd
+#ep1.pop(2)
+
+#del_cmnd
+#del ep1[4]
+
+print(ep1)
+#print(ep2)
