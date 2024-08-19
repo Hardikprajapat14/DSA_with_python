81cbac79d164ac1abae8f58f55c044fb9127dea4
Hardik Prajapat <135196687+Hardikprajapat14@users.noreply.github.com>
2024-08-19 12:38:06 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,19 @@
+""" name =  "Hardik"
+for i in name:
+    print(i)
+    if (i=="d"):
+        print ("he is nice boy")
+    else:
+        print("he is a bad boy")
+     """
+     
+""" colors = ["Red","blue","green","yellow"]
+for x in colors:
+    print (x)
+    if(x=="blue"):
+         print("this is my favourte color")
+    else:
+        print("i dont like") """
+        
+for k in range(10):
+    print (k)
